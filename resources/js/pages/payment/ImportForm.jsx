9e09a1528ea9a2ff8267e5510aa0a3607fc69694
HarshPatel0753 <harshpatel0753@gmail.com>
2025-08-07 16:49:27 -0400

    updated

--- resources/js/pages/payment/ImportForm.jsx
+++ resources/js/pages/payment/ImportForm.jsx
@@ -5,41 +5,45 @@ import AxiosInstance from "../../components/AxiosInstance.jsx";
 import { router } from "@inertiajs/react";
 
 const ImportForm = forwardRef(function ImportForm(props, ref) {
-    let modalRef = useRef(null);
+    const modalRef = useRef(null);
     const [file, setFile] = useState(null);
     const [errors, setErrors] = useState(null);
 
+    // Open modal and reset input
     function openModal() {
-        document.getElementById('file').value='';
+        document.getElementById("file").value = "";
+        setErrors(null);
         modalRef.current.show();
     }
 
-    const handelChange = (e) => {
-        setFile(e.target.files[0]);
-    };
-
+    // Close modal and reset state
     function closeModal() {
         setFile(null);
         setErrors(null);
     }
 
-    useImperativeHandle(ref, () => {
-        return {
-            openModal,
-            closeModal,
-        };
-    });
+    // Hook modal open/close methods to parent component
+    useImperativeHandle(ref, () => ({
+        openModal,
+        closeModal,
+    }));
 
-    function handleSubmit(e) {
+    // File input change handler
+    const handleChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    // Submit import form
+    const handleSubmit = (e) => {
         e.preventDefault();
-        // clearErrors();
-        AxiosInstance({
-            method: "post",
-            url: "payment/import",
-            data: { file: file },
+
+        const formData = new FormData();
+        formData.append("file", file);
+
+        AxiosInstance.post("payment/import", formData, {
             headers: {
                 Accept: "application/json",
-                "Content-type": "multipart/form-data",
+                "Content-Type": "multipart/form-data",
             },
         })
             .then((response) => {
@@ -47,22 +51,26 @@ const ImportForm = forwardRef(function ImportForm(props, ref) {
                 modalRef.current.close();
                 Swal.fire({
                     position: "top-end",
-                    title: Import File,
-                    text: ${response.data},
+                    title: "Import File",
+                    text: `${response.data}`,
                     icon: "success",
                     width: "400px",
                     showConfirmButton: false,
                     timer: 2000,
                 });
             })
-            .catch(function (error) {
-                setErrors(error.response.data.errors);
+            .catch((error) => {
+                if (error.response && error.response.data && error.response.data.errors) {
+                    setErrors(error.response.data.errors);
+                } else {
+                    console.error("Unexpected error:", error);
+                }
             });
-    }
+    };
 
     return (
         <Modal
-            id={"importPayment"}
+            id="importPayment"
             title={<h4 className="m-0 mt-2">Import Payment</h4>}
             modalSize="modal-lg"
             ref={modalRef}
@@ -76,22 +84,8 @@ const ImportForm = forwardRef(function ImportForm(props, ref) {
                 </button>
             }
             closeModal={closeModal}
-            closeModalButton={() => {
-                return false;
-            }}
+            closeModalButton={() => false}
         >
-            {/* <div className="">
-                <div className="mb-3">
-                    <label className="form-label control-label">File</label>
-                    <input
-                        type="file"
-                        name="profile_image_file"
-                        label="User Picture"
-                        onChange={() => {}}
-                        className={"form-control"}
-                    />
-                </div>
-            </div> */}
             <form className="form-group required">
                 <div className="gy-2">
                     <div className="mb-3">
@@ -100,42 +94,52 @@ const ImportForm = forwardRef(function ImportForm(props, ref) {
                             type="file"
                             name="file"
                             id="file"
-                            label="Import File"
-                            onChange={handelChange}
-                            className={"form-control"}
+                            onChange={handleChange}
+                            className="form-control"
                         />
-                        {errors && errors.file && (
+                        {/* Single field validation error */}
+                        {errors?.file && (
                             <div className="text-danger mt-1">
                                 {errors.file[0]}
                             </div>
                         )}
-                            {errors && !errors.file &&
-                                <div className="table table-responsive mt-4">
-                                    <table className="table table-bordered">
-                                        <thead>
-                                            <tr><th style={{ backgroundColor: '#ffbaba' }} colSpan='3' className="text-center">Errors</th></tr>
-                                            <tr>
-                                                <th scope="col">Row</th>
-                                                <th scope="col">Attribute</th>
-                                                <th scope="col">Errors</th>
+
+                        {/* Bulk/row-level validation errors */}
+                        {errors && !errors.file && Array.isArray(errors) && (
+                            <div className="table table-responsive mt-4">
+                                <table className="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th
+                                                colSpan="3"
+                                                className="text-center"
+                                                style={{ backgroundColor: "#ffbaba" }}
+                                            >
+                                                Errors
+                                            </th>
+                                        </tr>
+                                        <tr>
+                                            <th scope="col">Row</th>
+                                            <th scope="col">Attribute</th>
+                                            <th scope="col">Errors</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {errors.map((error, index) => (
+                                            <tr key={index}>
+                                                <td>{error.row}</td>
+                                                <td>{error.attribute}</td>
+                                                <td>
+                                                    {error.errors.map((msg, i) => (
+                                                        <h6 key={i} className="text-danger">{msg}</h6>
+                                                    ))}
+                                                </td>
                                             </tr>
-                                        </thead>
-                                        <tbody>
-                                            {errors && errors.map((error, index) => (
-                                                <tr key={index}>
-                                                    <td>{error.row}</td>
-                                                    <td>{error.attribute}</td>
-                                                    <td>
-                                                        {error.errors.map((subError, i) => (
-                                                            <h6 key={i} className="text-danger">{subError}</h6>
-                                                        ))}
-                                                    </td>
-                                                </tr>
-                                            ))}
-                                        </tbody>
-                                    </table>
-                                </div>
-                            }
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        )}
                     </div>
                 </div>
             </form>
